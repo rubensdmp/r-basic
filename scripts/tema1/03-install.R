a6e2191488bb5f1f0cf2ab804058bdc61fd86121
rubensdmp <rubensdmp@gmail.com>
2021-08-23 01:24:12 -0300

    Update 03-install.R

--- scripts/tema1/03-install.R
+++ scripts/tema1/03-install.R
@@ -1,5 +1,7 @@
+#Esto es para instalar paquetes:
 install.packages("tidyverse", dep = TRUE)
 
+#Esto es para importarlos
 library(tidyverse)
 
 install.packages("magic", dep = TRUE)
@@ -8,4 +10,6 @@ library(magic)
 
 magic(6)
 
+
+#Me muestra los paquetes instalados
 installed.packages()
